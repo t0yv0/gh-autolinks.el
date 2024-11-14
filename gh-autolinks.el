7dee0ebfe84eefcbe241e2b3199490b59b8f488c
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2024-11-13 20:01:16 -0500

    Add support for displaying PR title in an overlay
    Why this is nice: not editing the buffer text makes copying links back to GitHub quite easy.
    
    Relevant Emacs feature:
    
        https://www.gnu.org/software/emacs/manual/html_node/elisp/Overlays.html

--- gh-autolinks.el
+++ gh-autolinks.el
@@ -17,12 +17,17 @@
   :group 'gh-autolinks)
 
 
+(defcustom gh-autolinks-use-overlays nil
+  "Display title of the issue or pull request in an overlay."
+  :type 'boolean
+  :group 'gh-autolinks)
+
+
 ;;;###autoload
 (defun gh-autolinks-org-buffer ()
   "Detects GitHub references in the current org-mode buffer and
 automatically links them."
   (interactive)
-
   (save-excursion
     (goto-char (point-min))
     (while (re-search-forward (rx (group (or line-start " "))
@@ -37,11 +42,34 @@ automatically links them."
             (owner (match-string 3))
             (repo (match-string 4))
             (num (match-string 5)))
-        (if gh-autolinks-add-title
-            (replace-match (format "%s[[%s][%s/%s#%s: %s]]"
-                                   space url owner repo num (gh-autolinks--issue-or-pullreq-title url)))
-          (replace-match (format "%s[[%s][%s/%s#%s]]"
-                                 space url owner repo num)))))))
+        (cond
+         (gh-autolinks-use-overlays
+          (gh-autolinks--ensure-overlay
+           (match-beginning 2) ;; url
+           (match-end 5) ;; num
+           (lambda ()
+             (gh-autolinks--issue-or-pullreq-title url))))
+         (gh-autolinks-add-title
+          (replace-match (format "%s[[%s][%s/%s#%s: %s]]"
+                                 space url owner repo num
+                                 (gh-autolinks--issue-or-pullreq-title url))))
+         (t (replace-match (format "%s[[%s][%s/%s#%s]]"
+                                     space url owner repo num))))))))
+
+
+(defun gh-autolinks--ensure-overlay (beg end fetch-text)
+  "Adds an overlay to a region.
+
+First checks if there are existing overlays, in which case it
+does nothing as it assumes the work has been done already.
+
+Otherwise it displays the result of FETCH-TEXT after the region."
+  (when (null (overlays-in beg end))
+    (let ((new-overlay nil))
+      (setq new-overlay (make-overlay beg end))
+      (overlay-put new-overlay 'evaporate t)
+      (overlay-put new-overlay 'after-string
+                   (format ": %s" (funcall fetch-text))))))
 
 
 (defun gh-autolinks--issue-or-pullreq-title (url)
